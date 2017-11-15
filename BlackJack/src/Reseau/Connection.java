7ed7be80776022372f7a0563815fc74732d38f9b
vincent_valembois <vincent_valembois@ens.univ-artois.fr>
2017-11-15 22:48:26 +0100

    feat():Connection UDP and back server

--- BlackJack/src/Reseau/Connection.java
+++ BlackJack/src/Reseau/Connection.java
@@ -6,32 +6,65 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import Panel.Connect.Identification;
+
 
 public class Connection {
 	
-    public static void broadcast(String broadcastMessage, InetAddress address) throws IOException {
+	public static Connection share = new Connection();
+	
+	public  int port = 8080;
+	public  boolean recoit = true;
+	
+    public  void broadcast(String broadcastMessage, InetAddress address) throws IOException {
         DatagramSocket socket = new DatagramSocket();
         socket.setBroadcast(true);
  
         byte[] buffer = broadcastMessage.getBytes();
         
         DatagramPacket packet 
-          = new DatagramPacket(buffer, buffer.length, address, 8080);
+          = new DatagramPacket(buffer, buffer.length, address, port);
         socket.send(packet);
-        Connection.ecouteUDP();
         socket.close();
-        
     }
     
-    private static void ecouteUDP() {
+    private  void ecouteUDP(Identification ident) throws IOException {  		
+    	      DatagramSocket dsocket = new DatagramSocket(port);
+    	      byte[] buffer = new byte[50];
+
+    	      DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+    	      
+    	      Thread t = new Thread() {
+    	          public void run() {
+    	        	  	while (recoit) {
+    	      	        try {
+							dsocket.receive(packet);
+							String msg = new String(buffer, 0, packet.getLength());
+		    	      	      
+							
+		    	      	        ident.getListeServeur().add(packet.getAddress().getHostName(),port,msg);
+
+		    	      	        packet.setLength(buffer.length);
+					} catch (IOException e) {
+							e.printStackTrace();
+					}
+    	      	     }
+    	          }
+    	        };
+    	      t.start();
 	}
 
-	public static void creationBroadcast() {
+	public  void creationBroadcast(Identification ident) {
 		try {
-			Connection.broadcast("I WANT TO PLAY BLACKJACK", InetAddress.getByName("255.255.255.255"));
+			share.broadcast("I WANT TO PLAY BLACKJACK", InetAddress.getByName("255.255.255.255"));
+	        share.ecouteUDP(ident);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} 
     }
+
+	public void ferme() {
+		recoit = !recoit;
+	}
 
 }
