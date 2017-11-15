d57368444a79cabfad9e04c7acf5fa08ec8747a8
Loïck Magniez <loick_magniez@ens.univ-artois.fr>
2017-11-15 17:34:19 +0100

    feat(): plateau.c

--- ServeurBlackJack/lib/jeu.h
+++ ServeurBlackJack/lib/jeu.h
@@ -1,3 +1,6 @@
+#ifndef JEU_H
+#define JEU_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -6,16 +9,27 @@
 
 #define MAX_CARTE 10
 
+typedef enum {JOUE, SATISFAIT, PERDU} etat_jeu;
+
+
 typedef struct{
 	carte *cartes;
 	int nb_carte;
 	int valeur;
+	etat_jeu e_jeu;
 } jeu;
 
 void generer_jeu(jeu *j);
 void add_carte(jeu *j, carte new);
+carte remove_carte(jeu *j);
 void update_valeur_totale(jeu *j);
 int get_valeur_totale(jeu *j);
 int has_blackjack(jeu *j);
 void afficher_jeu(jeu *j);
 
+void start_jeu(jeu *j);
+void stop_jeu(jeu *j);
+void destroy_jeu(jeu *j);
+
+
+#endif
